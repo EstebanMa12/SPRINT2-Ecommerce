af1d4d7616f1d557ffbdd36162882b06b5a2d8b9
EstebanMa12 <desteban.maya@udea.edu.co>
2023-07-14 21:02:57 -0500

    Insert API of sneakers

---
+++ sneaks.js
@@ -0,0 +1,9 @@
+const SneaksAPI = require('sneaks-api');
+
+const sneaks = new SneaksAPI();
+
+sneaks.getProducts("Yeezy Cinder", 10, function(err, products){
+    console.log(products)
+})
+
+console.log(sneaks);
