ebdd3756e209bcf58c4b157baf319e249ac1da2f
EstebanMa12 <desteban.maya@udea.edu.co>
2023-07-15 23:53:16 -0500

    Implementing Sass

--- sneaks.js
+++
@@ -1,9 +0,0 @@
-const SneaksAPI = require('sneaks-api');
-
-const sneaks = new SneaksAPI();
-
-sneaks.getProducts("Yeezy Cinder", 10, function(err, products){
-    console.log(products)
-})
-
-console.log(sneaks);
